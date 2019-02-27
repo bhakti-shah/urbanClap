804b186cebd84eb72ad498ec69674eac84fb09ab
Sanchit Sahu <sanchit.sahu03@gmail.com>
2019-02-27 15:02:55 +0530

    Added cors access

--- app.js
+++ app.js
@@ -12,10 +12,23 @@ var app = express();
 var cors = require('cors');
 var mongoose = require('mongoose');
 
+allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if ('OPTIONS' === req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
